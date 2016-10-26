fc47ed224ac9f1432f73ede2bc50260b705de846
p sc <patty.stacruz@gmail.com>
2016-10-26 17:01:37 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  string = ''
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    string = array[0] + " and " + array[1]
+    return string
+  else
+    for i in 0..array.length-2
+      string = string + array[i] + ', '
+    end
+    string = string + "and " + array[array.length-1]
+  end
 end
